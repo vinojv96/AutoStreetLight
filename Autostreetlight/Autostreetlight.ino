3d1abf3a42310ddbe835f1932f1b2a8421ccda71
Vinothkumar Jayaraman <vinojv96@gmail.com>
2020-11-26 21:11:11 +0530

    code added

---
+++ Autostreetlight/Autostreetlight.ino
@@ -0,0 +1,27 @@
+/*
+********** github.com/vinojv96 *****
+*/
+int sensorPin = A0;
+int sensorValue = 0;
+
+
+void setup() {
+pinMode(10,OUTPUT);
+Serial.begin(9600);
+
+}
+
+void loop() {
+sensorValue = analogRead(sensorPin);
+Serial.println(sensorValue);
+if(sensorValue > 800) // Adjust the sensor value if needed
+{
+  Serial.println("led on");
+  digitalWrite(10, HIGH);
+  delay(30000);//this delay avoids flickering of light in the dusk or dawn time
+}
+else
+digitalWrite(10, LOW);
+delay(sensorValue);
+}
+
